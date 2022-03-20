b4e5ac92328d7860137aa526aadfaf9d240179d1
Tanmoy2099 <87271422+Tanmoy2099@users.noreply.github.com>
2022-03-20 22:30:54 +0530

    practice

---
+++ script.js
@@ -0,0 +1,65 @@
+const data = {
+  mainTitle: "My Favourite Travel Destinations",
+  cardContents: [
+    {
+      name: "Bali",
+      imageLink:
+        "https://images.unsplash.com/photo-1577717903315-1691ae25ab3f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+      name: "Montreux",
+      imageLink:
+        "https://images.unsplash.com/photo-1581541709890-f566353a3807?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1575&q=80",
+    },
+    {
+      name: "Venice",
+      imageLink:
+        "https://images.unsplash.com/photo-1558271736-cd043ef2e855?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80",
+    },
+    {
+      name: "Germany",
+      imageLink:
+        "https://images.unsplash.com/photo-1515488764276-beab7607c1e6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=850&q=80",
+    },
+  ],
+  userData: { name: "Tanmoy", tagLine: "Baby steps into the world of web!" },
+};
+
+
+// Plug in title data to HTML
+let mainTitleElement = document.getElementById("main-title");
+mainTitleElement.innerHTML += data.mainTitle;
+
+
+// Create template for card content
+let getCardStructure = (name, imageLink) => {
+  return `<div class="col-12 col-sm-6 mb-4">
+  <div class="tile">
+    <div class="tile-text text-center">
+      <h5>${name}</h5>
+    </div>
+    <img 
+      src="${imageLink}" />
+  </div>
+</div>`;
+};
+
+let cardRow = document.getElementById("card-row");
+
+// Plug in card data to HTML
+data.cardContents.forEach((cardContent) => {
+  let { name, imageLink } = cardContent;
+  cardRow.innerHTML += getCardStructure(name, imageLink);
+});
+
+// Create template for footer content
+let getFooterStructure = (name, tagLine) => {
+  return `<h3 class="me-3"><i class="fas fa-heart"></i> ${name}</h3>
+    <p>${tagLine}</p>`;
+};
+
+let footerContent = document.getElementById("footer");
+let { name, tagLine } = data.userData;
+
+// Plug in footer data to HTML
+footerContent.innerHTML += getFooterStructure(name, tagLine);
